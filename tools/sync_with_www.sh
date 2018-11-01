58f1a56b2885e68db48888c1ba06b75b662f27e3
windic <esc00019@gmail.com>
2018-11-01 19:44:23 +0100

    modificación y simplificación de sync file

--- tools/sync_with_www.sh
+++ tools/sync_with_www.sh
@@ -1,21 +1,2 @@
-#!/bin/sh
-# Syncs (with rsync) the source and destination directories
-
-#user
-USER=pi
-#Name of the project
-PROJECT=pabli_chat
-
-# Destination directory
-DEST_DIR=/var/www/html/$PROJECT
-# Source directory
-SRC_DIR=/home/$USER/$PROJECT
-
-
-# Exclude directory
-EXCLUDE_DIR=fuelphp/fuel/app/logs
-EXCLUDE_DIR2=fuelphp/fuel/app/uploads
-
-
 # Do the actual sync
-sudo rsync -avz --chmod g+rX --delete $SRC_DIR $DEST_DIR
+sudo rsync -avz --delete /home/pi/pabli_chat/pabli_chat /var/www/html/back_mosquito
